ac7c30498ea5fb0018eb7283843836817986e435
Edgar <muchemiedgar2@gmail.com>
2025-03-03 14:31:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Create the welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create the excited welcome message (all uppercase)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create the short greeting with just the first initial
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
